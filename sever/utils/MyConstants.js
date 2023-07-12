1fdedd89c11829062f6b3b851734ab96ad7266a3
phatlatxoai <ntplakao1232gmail.com>
2023-07-12 10:51:19 +0700

    sad
    ad

---
+++ sever/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "thuongmaidientu.tptqeep.mongodb.net",
+  DB_USER: "admin",
+  DB_PASS: "12345",
+  DB_DATABASE: "ShopingDatabase",
+  EMAIL_USER: "ntplakao123@gmail.com", // gmail mail service
+  EMAIL_PASS: "rrlghwcvvmmbxqvt",
+  JWT_SECRET: "abcsdlakjsd",
+  JWT_EXPIRES: "10000000", // in milliseconds
+};
+module.exports = MyConstants;
